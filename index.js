182bee94e485cf134efed23d4ea644c3e4139474
Angus Alves <angustalves@gmail.com>
2026-07-01 21:53:33 -0400

    changing to npm salesforce cli install

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ try {
 }
 
 function installSFDX(){
-  var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz -q -P /tmp'
+  /*var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz -q -P /tmp'
   var createDir = 'mkdir /tmp/sfdx'
   var unzip = 'tar xJf /tmp/sfdx-linux-x64.tar.xz -C /tmp/sfdx --strip-components 1'
   var install = 'echo "/tmp/sfdx/bin" >> $GITHUB_PATH'
   var version = '/tmp/sfdx/bin/sfdx --version && /tmp/sfdx/bin/sfdx plugins --core'
   exec(download+' && '+createDir+' && '+unzip+' && '+install+' && '+version, function(error, stdout, stderr){
+    if(error) throw(stderr)
+    core.info(stdout)
+    if(core.getInput('sfdx-auth-url')) createAuthFile()
+  })*/
+  exec('npm i -g @salesforce/cli@nightly', function(error, stdout, stderr){
     if(error) throw(stderr)
     core.info(stdout)
     if(core.getInput('sfdx-auth-url')) createAuthFile()
